c1b4214737d7a89e032e1860ff6c383b6e20c85e
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-28 14:36:19 +0500

    feat(todo): add match-case menu, Showtask function, and optimize task display

--- todo-app.py
+++ todo-app.py
@@ -1,22 +1,24 @@
-tasks= []
-n=1
 #tasks
-if tasks:
-    for i in tasks:
-        print(f"{n}. {i}")
-        n=n+1
-    
+tasks= []
+
+# show function
+def Showtask():
+    n=1
+    if tasks:
+        for i in tasks:
+            print(f"{n}. {i}")
+            n=n+1
     print("_" * 15)
 
-
-def addtask():
+# add-task function
+def Addtask():
     ntask = input("Enter Task: ")
     tasks.append(ntask)
     print("task added.")
-    for i in range(len(tasks)):
-        print(f"{i+1} = {tasks[i]}")
-
-def removetask():
+    # calling show function
+    Showtask();
+# remove-task function
+def Removetask():
     if not tasks:
         print("No Task available to remove.")
     else:
@@ -24,19 +26,31 @@ def removetask():
             i = int(input("which position to remove: "))
             tasks.pop(i-1)
             print("task removed.")
-            for i in range(len(tasks)):
-                print(f"{i+1} = {tasks[i]}")
+            # calling show function
+            Showtask();
         except (ValueError, IndexError):
             print("The value is out of range.")
-
+# Infinite Loop
 while True:
     try:
-        choice = int(input("1.Add Task\n2.Remove Task\n Enter your choice: "))
-        if(choice == 1):
-            addtask()
-        elif(choice ==2):
-            removetask()
+        choice = int(input("0. Exit\n1. Add Task\n2. Remove Task\n3. Display Task\nEnter your choice: "))
+        # if conditional: continue if choice is smaller than 4.
+        if(choice<4):
+            match choice:
+                case 1:
+                    # calling add-task
+                    Addtask()
+                case 2:
+                    # calling remove-task
+                    Removetask()
+                case 3:
+                    # calling show-task
+                    Showtask()
+                # default case
+                case _:
+                # keyword to stop the loop.
+                    break
         else:
-            break
+            print("Enter value between 0-3. 0 for exit.")
     except ValueError:
         print("Enter a integer!")
